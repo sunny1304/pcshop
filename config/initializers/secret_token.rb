61121ea0f7ec9ab2403c47248f6701aa37ea3cc6
sunny1304 <sunny1304.ad1@gmail.com>
2013-07-10 00:06:49 +0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pcshop::Application.config.secret_token = '6d2787fd8d7e518fe62111ee017b938a1edddbde4ff5e93f9e06a8385baae1f5ba0ce414fa73268e101e6dc4d01d2c7e40576f7bf30df7f4637f66a198b37497'
